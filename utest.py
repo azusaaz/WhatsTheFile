b82a4d3f1c2e69e9ea1c860bea028fe52039f667
azusaaz <ashimazaki@myseneca.ca>
2017-12-25 23:58:22 -0500

    uni10

--- utest.py
+++ utest.py
@@ -9,13 +9,13 @@ class Test(unittest.TestCase):
     def testName(self):
         a = whatsTheFile
         filepath = "./mydata.txt"
-        self.assertEqual(a.wtf.getFileName(filepath),mydata.txt)
+        self.assertEqual(a.wtf.getFileName(filepath),"mydata.txt")
                          
 
     def testSize(self):
         a = whatsTheFile
         filepath = "./mydata.txt"
-        self.assertEqual(a.wtf.getFileSize(filepath),"44")
+        self.assertEqual(a.wtf.getFileSize(filepath),"43")
 
 if __name__ == '__main__':
     unittest.main()
