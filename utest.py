c58c360764178e6a7120bab420e2cf289299f98c
azusaaz <ashimazaki@myseneca.ca>
2017-12-26 20:03:51 -0500

    utest update

--- utest.py
+++ utest.py
@@ -20,12 +20,12 @@ class Test(unittest.TestCase):
     def testSize(self):
         a = whatsTheFile
         filepath = "./mydata.txt"
-        self.assertEqual(a.wtf.to_md5(filepath), "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12")
+        self.assertEqual(a.wtf.to_md5(filepath),"9e107d9d372bb6826bd81d3542a419d6")
 
     def testSize(self):
         a = whatsTheFile
         filepath = "./mydata.txt"
-        self.assertEqual(a.wtf.to_sha1(filepath) ,"9e107d9d372bb6826bd81d3542a419d6")
+        self.assertEqual(a.wtf.to_sha1(filepath),"2fd4e1c67a2d28fced849ee1bb76e7391b93eb12")
 
 if __name__ == '__main__':
     unittest.main()
